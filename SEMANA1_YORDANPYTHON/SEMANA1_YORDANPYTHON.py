010cb75169d125f8d59e240a34ad43e73a3016bf
Leandro <leandrofloresaquino@gmail.com>
2025-09-02 12:08:46 -0500

    subiendo ejercicio 1

---
+++ SEMANA1_YORDANPYTHON/SEMANA1_YORDANPYTHON.py
@@ -0,0 +1,7 @@
+﻿def ejer1():
+    nombre = input("Ingrese su nombre: ")
+    carrera = input("Ingrese su carrera: ")
+
+    print(f"\n{nombre}, bienvenido al curso de fundamentos de algoritmos de la carrera {carrera}")
+
+ejer1()
---
+++ SEMANA3YORDAN/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA3YORDAN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey(); //detenimiento de consola
+        }
+        
+        static void ejer1()
+        {
+            string nombre, carrera;
+
+            Console.Write("Ingresa su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingresa su carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, bienvenido al curso de fundamentos de algoritmos de la {carrera}");
+        }
+
+        static void ejer2()
+        {
+
+        }
+
+        static void ejer3()
+        {
+
+        }
+
+        static void ejer4()
+        {
+
+        }
+
+        static void ejer5()
+        {
+
+        }
+
+        static void ejer6()
+        {
+
+        }
+    }
+}
